1893be94d0887039d5eba321e139cd54f311a8af
Ovgiliot <ovgiliot@gmail.com>
2026-02-13 22:57:34 +0200

    Added graphics drivers

--- modules/system/desktop.nix
+++ modules/system/desktop.nix
@@ -65,5 +65,21 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but good fallback)
+      libvdpau-va-gl
+      vpl-gpu-rt # For newer Intel GPUs (Gen11+) VPL/QSV
+    ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [
+      intel-media-driver
+      intel-vaapi-driver
+      libvdpau-va-gl
+      vpl-gpu-rt
+    ];
+  };
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
   };
 }
