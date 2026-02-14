b8c3134ae5d6abf7addfeed30c2b77010be0250e
Ovgiliot <ovgiliot@gmail.com>
2026-02-14 17:22:49 +0200

    added bt to wofi

--- home/ovg/default.nix
+++ home/ovg/default.nix
@@ -85,6 +85,55 @@
           fi
       fi
     '')
+
+    (pkgs.writeShellScriptBin "bluetooth-menu" ''
+      power_on=$(bluetoothctl show | grep "Powered: yes" | wc -l)
+      if [ "$power_on" -eq 0 ]; then
+        action=$(echo -e "Power On\nExit" | wofi -dmenu -p "Bluetooth is Power Off" -i)
+        if [ "$action" == "Power On" ]; then
+          bluetoothctl power on
+        else
+          exit
+        fi
+      fi
+
+      devices=$(bluetoothctl devices | cut -d ' ' -f 2-)
+      device_list=""
+      while read -r line; do
+        mac=$(echo "$line" | cut -d ' ' -f 1)
+        name=$(echo "$line" | cut -d ' ' -f 2-)
+        info=$(bluetoothctl info "$mac")
+        connected=$(echo "$info" | grep "Connected: yes" | wc -l)
+        if [ "$connected" -eq 1 ]; then
+          device_list+="CONNECTED: $name ($mac)\n"
+        else
+          device_list+="$name ($mac)\n"
+        fi
+      done <<< "$devices"
+
+      device_list+="Scan for devices\nPower Off"
+
+      chosen=$(echo -e "$device_list" | wofi -dmenu -p "Bluetooth Devices" -i)
+
+      if [ -n "$chosen" ]; then
+        if [ "$chosen" == "Scan for devices" ]; then
+          notify-send "Bluetooth" "Scanning for 15 seconds..."
+          bluetoothctl scan on &
+          sleep 15
+          bluetoothctl scan off
+          bluetooth-menu
+        elif [ "$chosen" == "Power Off" ]; then
+          bluetoothctl power off
+        else
+          mac=$(echo "$chosen" | sed 's/.*(\(.*\))/\1/')
+          if [[ "$chosen" == CONNECTED:* ]]; then
+            bluetoothctl disconnect "$mac"
+          else
+            bluetoothctl connect "$mac" || (bluetoothctl pair "$mac" && bluetoothctl connect "$mac")
+          fi
+        fi
+      fi
+    '')
   ];
 
   # Git configuration
