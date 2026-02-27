c9d7366b2292d4f9c2f3002e5bc484d85c213426
Ovgiliot <ovgiliot@gmail.com>
2026-02-27 17:43:56 +0200

    nvim: remove incorrect --[[@as ev.buf]] annotations from DAP functions

--- home/ovg/nvim/lua/ovg/plugins.lua
+++ home/ovg/nvim/lua/ovg/plugins.lua
@@ -493,16 +493,16 @@ require("lazy").setup({
 
 			dapui.setup()
 
-			dap.listeners.before.attach.dapui_config = function() --[[@as ev.buf]]
+			dap.listeners.before.attach.dapui_config = function()
 				dapui.open()
 			end
-			dap.listeners.before.launch.dapui_config = function() --[[@as ev.buf]]
+			dap.listeners.before.launch.dapui_config = function()
 				dapui.open()
 			end
-			dap.listeners.before.event_terminated.dapui_config = function() --[[@as ev.buf]]
+			dap.listeners.before.event_terminated.dapui_config = function()
 				dapui.close()
 			end
-			dap.listeners.before.event_exited.dapui_config = function() --[[@as ev.buf]]
+			dap.listeners.before.event_exited.dapui_config = function()
 				dapui.close()
 			end
             
@@ -517,7 +517,7 @@ require("lazy").setup({
                     name = "Launch",
                     type = "gdb",
                     request = "launch",
-                    program = function() --[[@as ev.buf]]
+                    program = function()
                         return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
                     end,
                     cwd = "${workspaceFolder}",
