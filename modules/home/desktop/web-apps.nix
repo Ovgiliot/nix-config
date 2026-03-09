b86bca057b0f752d46d199f8bcd7ca59b71a3fc1
Ovgiliot <ovgiliot@gmail.com>
2026-03-09 20:46:46 +0200

    betaflight?

--- modules/home/desktop/web-apps.nix
+++ modules/home/desktop/web-apps.nix
@@ -19,6 +19,11 @@
       icon = "audio-x-generic";
     }
     {
+      name = "Betaflight";
+      url = "https://app.betaflight.com";
+      icon = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.wrdMW93WN-mqGwFE1yu8BQAAAA%3Fpid%3DApi&f=1&ipt=4c96b08ab4306ba377bd47d8d4e1c647a6e6667bfcf592b20c7f6096538e277a";
+    }
+    {
       name = "Nix Packages Search";
       url = "https://search.nixos.org/packages";
       icon = "system-software-install";
