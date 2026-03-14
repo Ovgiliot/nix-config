e003b92a87a1e138c77aa5c8f2c3d49f683588af
Ovgiliot <ovgiliot@gmail.com>
2026-03-14 19:18:25 +0200

    Automated config update

--- modules/system/desktop/audio.nix
+++ modules/system/desktop/audio.nix
@@ -1,4 +1,8 @@
-{...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   # Disable PulseAudio in favor of PipeWire
   services.pulseaudio.enable = false;
 
@@ -11,4 +15,11 @@
     # JACK support is added by the music workflow when needed.
     # wireplumber is the default session manager since NixOS 23.05; no need to declare it.
   };
+
+  # Expose GStreamer plugin path so pre-built apps (and any app using
+  # GStreamer outside of nix wrappers) can discover audio sinks.
+  environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPath "lib/gstreamer-1.0" [
+    pkgs.gst_all_1.gst-plugins-base
+    pkgs.gst_all_1.gst-plugins-good
+  ];
 }
